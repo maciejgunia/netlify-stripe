dc41574b7983806e502eabe87da744cbdea70f7d
Maciej Gunia <maciej.gunia@gmail.com>
2021-10-30 19:58:33 +0200

    use context

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,10 @@
 console.log(process.env);
 export const baseUrl =
-    process.env.NODE_ENV === "development" ? "http://localhost:8888" : process.env.REACT_APP_DEPLOY_URL;
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:8888"
+        : process.env.CONTEXT === "production"
+        ? process.env.REACT_APP_URL
+        : process.env.REACT_APP_DEPLOY_URL;
 export const createPaymentUrl = `${baseUrl}/.netlify/functions/create-payment`;
 export const getProductsUrl = `${baseUrl}/.netlify/functions/products`;
 export const imagesUrl = process.env.NODE_ENV === "development" ? "http://localhost:8888" : "https://taku.imgix.net";
