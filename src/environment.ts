eeb68e48a8b6eb12a121bf07723c4f2a5f1f4c9c
Maciej Gunia <maciej.gunia@gmail.com>
2021-10-30 19:38:38 +0200

    edd env

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,6 @@
 console.log(process.env);
-export const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:8888" : process.env.DEPLOY_URL;
+export const baseUrl =
+    process.env.NODE_ENV === "development" ? "http://localhost:8888" : process.env.REACT_APP_DEPLOY_URL;
 export const createPaymentUrl = `${baseUrl}/.netlify/functions/create-payment`;
 export const getProductsUrl = `${baseUrl}/.netlify/functions/products`;
 export const imagesUrl = process.env.NODE_ENV === "development" ? "http://localhost:8888" : "https://taku.imgix.net";
