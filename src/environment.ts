28419e8833fa1b587d52c478425a34cb0b78b2eb
Maciej Gunia <maciej.gunia@gmail.com>
2021-10-30 19:22:01 +0200

    test env

--- src/environment.ts
+++ src/environment.ts
@@ -1,3 +1,4 @@
+console.log(process.env);
 export const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:8888" : process.env.DEPLOY_URL;
 export const createPaymentUrl = `${baseUrl}/.netlify/functions/create-payment`;
 export const getProductsUrl = `${baseUrl}/.netlify/functions/products`;
