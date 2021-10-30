ad0817a82bb39333b99faeedfaf161b6a84844fd
Maciej Gunia <maciej.gunia@gmail.com>
2021-10-30 20:02:47 +0200

    use proper var this time

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ console.log(process.env);
 export const baseUrl =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8888"
-        : process.env.CONTEXT === "production"
+        : process.env.REACT_APP_CONTEXT === "production"
         ? process.env.REACT_APP_URL
         : process.env.REACT_APP_DEPLOY_URL;
 export const createPaymentUrl = `${baseUrl}/.netlify/functions/create-payment`;
