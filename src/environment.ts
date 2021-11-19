a7494d4612d2eee7834427c9893b26d912021b45
Maciej Gunia <maciej.gunia@gmail.com>
2021-11-19 12:54:56 +0100

    log env

--- src/environment.ts
+++ src/environment.ts
@@ -7,3 +7,10 @@ export const baseUrl =
 export const createPaymentUrl = `${baseUrl}/.netlify/functions/create-payment`;
 export const getProductsUrl = `${baseUrl}/.netlify/functions/products`;
 export const imagesUrl = process.env.NODE_ENV === "development" ? "http://localhost:8888" : "https://taku.imgix.net";
+
+console.log(
+    process.env.REACT_APP_CONTEXT,
+    process.env.REACT_APP_URL,
+    process.env.REACT_APP_DEPLOY_PRIME_URL,
+    process.env
+);
